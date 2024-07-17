3c1cf422b33114129fd96da9879222f439224f7b
linlin deng <watermelon00315@gmail.com>
2024-07-17 15:07:35 +1000

    add user migration

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -1,22 +1,24 @@
-namespace Sample.Test.Domain.Entities;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sample.Test.Domain.Entities
 {
-    public class User
+    public class User : BaseAuditableEntity
     {
         public int UserID { get; set; }
 
-        [StringLength(250)]
-        public string FirstName { get; set; }
+        [MaxLength(250)]
+        public string? FirstName { get; set; }
 
-        [StringLength(250)]
-        public string LastName { get; set; }
+        [MaxLength(250)]
+        public string? LastName { get; set; }
 
-        [StringLength(250)]
-        public string Email { get; set; }
+        [MaxLength(250)]
+        public string? Email { get; set; }
 
         public DateTime DateAdded { get; set; }
     }
 }
-
 
 
 
